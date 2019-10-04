43302b6e21726c3793d5e87762492d78744592b7
Ritik Nandwal <48522685+nandwalritik@users.noreply.github.com>
2019-10-04 17:48:37 +0530

    Added arrayOut.java

---
+++ src/ArrayOut.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ArrayOut{
+	public static void main(String args[]){
+	try{
+		int a[] = new int[5];
+		a[6]=9;
+	}
+	catch(ArrayIndexOutOfBoundsException e){
+		System.out.println("Array index is out of bounds");
+	
+	}
+	
+	
+	}
+
+
+}
